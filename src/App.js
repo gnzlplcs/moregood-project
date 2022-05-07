8a4d40726e9b74ae980ad32323d896c5de10c22c
gnzlplcs <palaciosgm@outlook.com>
2022-05-06 22:46:53 -0500

    feat: baseUrl, workspace and API key work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 
+const baseUrl = "https://moregooder-api-stage.herokuapp.com";
+const workspaceId = process.env.REACT_APP_WORKSPACE_ID;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const parentType = "contact";
+const query = "data=trait";
+
 function App() {
+  const url = `${baseUrl}/${workspaceId}/${parentType}?${query}`;
+
+
+
   return (
     <div className="App">
       <h1>MoreGood Project</h1>
