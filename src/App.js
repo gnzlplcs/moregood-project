82759ac3b8c4edcdb01836f78f6fb1d9178ec326
gnzlplcs <palaciosgm@outlook.com>
2022-05-13 21:52:16 -0500

    ✅ create an Id collection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { queryUrl } from "./utils/url";
 import customHeaders from "./utils/customHeaders";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -13,15 +14,24 @@ function App() {
 
   const getData = () => {
     axios(queryUrl, customHeaders)
-      .then((res) => setRawData(res.data.body))
-      .catch((err) => console.log(`Error fetching data: ${err}`));
+    .then((res) => setRawData(res.data.body))
+    .catch((err) => console.log(`Error fetching data: ${err}`));
   };
 
+  const getId = rawData.map(item => item.id)
+
+  const [idCollection, setIdCollection] = useState([])
+  useEffect(() => {
+    setIdCollection(getId)
+  }, [rawData])
+
   console.log(rawData)
+  console.log(idCollection)
 
   return (
     <div className="App">
       <h1>Contacts</h1>
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = (props) => {
+  return <div>Future Contact</div>;
+};
+
+export default Contact;
